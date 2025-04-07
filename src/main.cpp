9294e4e817dfff4348f44b9724495eb4a4e94e86
Rosendo Martinez <rosendomartinez270@gmail.com>
2025-04-07 16:22:46 -0500

    clean up / refactor

--- src/main.cpp
+++ src/main.cpp
@@ -15,30 +15,44 @@ const Vec3f BLACK (0.0f, 0.0f, 0.0f);
 const TGAColor TGA_WHITE = TGAColor(255, 255, 255, 255);
 const TGAColor TGA_RED   = TGAColor(255, 0,   0,   255);
 const TGAColor TGA_GREEN = TGAColor(0,   255, 0,   255);
-const int width  = 1000;
-const int height = 1000;
+
+const int   device_width  = 1000;
+const int   device_height = 1000;
 const float virtual_screen_width = 0.65;
 const float virtual_screen_height = 0.65;
 
+struct TrianglePixel
+{
+    Vec2i pixel;
+    Vec3f barycentric;
+};
+
+
+struct LinePixel
+{
+    Vec2i pixel;
+    float t;
+};
+
+
 TGAColor to_tgacolor(Vec3f color);
-// void draw_line(Vec2i p0, Vec2i p1, TGAImage& image, Vec3f color);
-void draw_line(Vec3f p0, Vec3f p1, TGAImage& image, Vec3f color);
-// void draw_triangle(Vec3f a, Vec3f b, Vec3f c, TGAImage& image, Vec3f a_color, Vec3f b_color, Vec3f c_color, float** z_buffer, TGAImage& texture);
-void draw_triangle(Vec3f a, Vec3f b, Vec3f c, TGAImage& image, Vec2f a_uv, Vec2f b_uv, Vec2f c_uv, float** z_buffer, TGAImage& texture);
+Vec3f to_vec3fcolor(TGAColor color);
+std::vector<LinePixel> rasterize_line(Vec2f p0, Vec2f p1, float thickness = 1.0f);
+std::vector<TrianglePixel> rasterize_triangle(Vec2f a, Vec2f b, Vec2f c);
 
 int main()
 {   
-    TGAImage image(width, height, TGAImage::RGB);
+    TGAImage image(device_width, device_height, TGAImage::RGB);
     TGAImage texture;
     texture.read_tga_file("./obj/african_head_diffuse.tga");
     texture.flip_vertically();
     Model model ("./obj/african_head.obj");
 
-    float** z_buffer = new float*[height];
-    for (int i = 0; i < height; i++)
+    float** z_buffer = new float*[device_height];
+    for (int i = 0; i < device_height; i++)
     {
-        z_buffer[i] = new float[width];
-        for (int j = 0; j < width; j++)
+        z_buffer[i] = new float[device_width];
+        for (int j = 0; j < device_width; j++)
         {
             z_buffer[i][j] = -std::numeric_limits<float>::max();
         }
@@ -50,75 +64,91 @@ int main()
         Vec3f v0 = model.vert(face[0]);
         Vec3f v1 = model.vert(face[3]);
         Vec3f v2 = model.vert(face[6]);
-
+        Vec3f n0 = model.norm(face[2]);
+        Vec3f n1 = model.norm(face[5]);
+        Vec3f n2 = model.norm(face[8]);
         Vec2f uv0 = model.uv(face[1]);
         Vec2f uv1 = model.uv(face[4]);
         Vec2f uv2 = model.uv(face[7]);
 
         // Back facing triangle check
-        if (get_triangle_normal(v0, v1, v2) * Vec3f(0.0f, 0.0f, 1.0f) <= 0.0f)
+        if (get_triangle_normal(v0, v1, v2) * Vec3f(0.0f, 0.0f, 1.0f) <= 0.0f) // this is fine for now because the normals are the same in local, global, and camera
         {
             continue;
         }
 
-        // // pretty much local --> global --> screen space --> pixel space
-        // Vec2i v0_trans ((v0.x + 1.0f) * ((width - 1)/ 2.0f), (v0.y + 1.0f) * ((height - 1) / 2.0f));
-        // Vec2i v1_trans ((v1.x + 1.0f) * ((width - 1)/ 2.0f), (v1.y + 1.0f) * ((height - 1) / 2.0f));
-        // Vec2i v2_trans ((v2.x + 1.0f) * ((width - 1)/ 2.0f), (v2.y + 1.0f) * ((height - 1) / 2.0f));
+        // Transformations: local --> global --> camera --> virtual screen --> device screen
 
-        // transformation: local --> global
-            // used to scale, rotate, postion object in world
         Vec3f v0_global = v0;
         Vec3f v1_global = v1;
         Vec3f v2_global = v2;
-        // transformation: global --> camera
-            // used to get camera view of world
-        Vec3f v0_cam = v0_global + Vec3f(0.0f, 0.0f, -2.0f);
-        Vec3f v1_cam = v1_global + Vec3f(0.0f, 0.0f, -2.0f);
-        Vec3f v2_cam = v2_global + Vec3f(0.0f, 0.0f, -2.0f);
-        // transformation: camera --> projection to virtual screen
-            // project 3d world onto virtual screen
-        Vec3f v0_virtual = v0_cam;
-        Vec3f v1_virtual = v1_cam;
-        Vec3f v2_virtual = v2_cam;
-        // transformation: virtual screen --> device coordinates
-            // map virtual screen to actual device screen
-            // virtual_screen is 2x2, origin is center of it
-        float x_scale = (width / virtual_screen_width) / 2.0f;
-        float y_scale = (height / virtual_screen_height) / 2.0f;
-        Vec3f v0_device = Vec3f((v0_virtual.x * x_scale) + (width / 2.0f), (v0_virtual.y * y_scale) + (height / 2.0f), v0_virtual.z);
-        Vec3f v1_device = Vec3f((v1_virtual.x * x_scale) + (width / 2.0f), (v1_virtual.y * y_scale) + (height / 2.0f), v1_virtual.z);
-        Vec3f v2_device = Vec3f((v2_virtual.x * x_scale) + (width / 2.0f), (v2_virtual.y * y_scale) + (height / 2.0f), v2_virtual.z);
+        Vec3f v0_camera = v0_global + Vec3f(0.0f, 0.0f, -2.0f);
+        Vec3f v1_camera = v1_global + Vec3f(0.0f, 0.0f, -2.0f);
+        Vec3f v2_camera = v2_global + Vec3f(0.0f, 0.0f, -2.0f);
+        Vec3f v0_virtual = v0_camera;
+        Vec3f v1_virtual = v1_camera;
+        Vec3f v2_virtual = v2_camera;
+        Vec3f v0_device = Vec3f((v0_virtual.x * ((device_width / virtual_screen_width) / 2.0f)) + (device_width / 2.0f), (v0_virtual.y * ((device_height / virtual_screen_height) / 2.0f)) + (device_height / 2.0f), v0_virtual.z);
+        Vec3f v1_device = Vec3f((v1_virtual.x * ((device_width / virtual_screen_width) / 2.0f)) + (device_width / 2.0f), (v1_virtual.y * ((device_height / virtual_screen_height) / 2.0f)) + (device_height / 2.0f), v1_virtual.z);
+        Vec3f v2_device = Vec3f((v2_virtual.x * ((device_width / virtual_screen_width) / 2.0f)) + (device_width / 2.0f), (v2_virtual.y * ((device_height / virtual_screen_height) / 2.0f)) + (device_height / 2.0f), v2_virtual.z);
         
-        // send to rasterizer
+        // Shading / colors
 
-        Vec3f a_color;
-        a_color.x = (std::rand() % 101) / 100.0f;
-        a_color.y = (std::rand() % 101) / 100.0f;
-        a_color.z = (std::rand() % 101) / 100.0f;
-        Vec3f b_color;
-        b_color.x = (std::rand() % 101) / 100.0f;
-        b_color.y = (std::rand() % 101) / 100.0f;
-        b_color.z = (std::rand() % 101) / 100.0f;
-        Vec3f c_color;
-        c_color.x = (std::rand() % 101) / 100.0f;
-        c_color.y = (std::rand() % 101) / 100.0f;
-        c_color.z = (std::rand() % 101) / 100.0f;
+        Vec3f rnd_color_1;
+        rnd_color_1.x = (std::rand() % 101) / 100.0f;
+        rnd_color_1.y = (std::rand() % 101) / 100.0f;
+        rnd_color_1.z = (std::rand() % 101) / 100.0f;
+        Vec3f rnd_color_2;
+        rnd_color_2.x = (std::rand() % 101) / 100.0f;
+        rnd_color_2.y = (std::rand() % 101) / 100.0f;
+        rnd_color_2.z = (std::rand() % 101) / 100.0f;
+        Vec3f rnd_color_3;
+        rnd_color_3.x = (std::rand() % 101) / 100.0f;
+        rnd_color_3.y = (std::rand() % 101) / 100.0f;
+        rnd_color_3.z = (std::rand() % 101) / 100.0f;
 
-        // Shading
-        Vec3f normal = get_triangle_normal(v0, v1, v2);
+        Vec3f normal = get_triangle_normal(v0, v1, v2); // this is fine for now because the normals are the same in local, global, and camera
         Vec3f light_dir = Vec3f(0.0f, 0.0f, 1.0f);
         float ambient = 0.01f;
-        float diffuse = normal * light_dir;
-        Vec3f flat_gray_shading = clampedVec3f(Vec3f(diffuse + ambient), 0.0f, 1.0f);
+        float diffuse_gouraud = normal * light_dir;
 
-        // a_color = clampedVec3f((a_color * diffuse) + Vec3f(ambient), 0.0f, 1.0f); 
-        // b_color = clampedVec3f((b_color * diffuse) + Vec3f(ambient), 0.0f, 1.0f); 
-        // c_color = clampedVec3f((c_color * diffuse) + Vec3f(ambient), 0.0f, 1.0f);    
-        
-        // draw_triangle(v0_device, v1_device, v2_device, image, flat_gray_shading, flat_gray_shading, flat_gray_shading, z_buffer);
-        // draw_triangle(v0_device, v1_device, v2_device, image, a_color, b_color, c_color, z_buffer);
-        draw_triangle(v0_device, v1_device, v2_device, image, uv0, uv1, uv2, z_buffer, texture);
+        Vec3f flat_gray_shaded_gouraud = clampedVec3f(Vec3f(diffuse_gouraud + ambient), 0.0f, 1.0f);
+        Vec3f rnd_color_1_shaded_gouraud = clampedVec3f((rnd_color_1 * diffuse_gouraud) + Vec3f(ambient), 0.0f, 1.0f); 
+        Vec3f rnd_color_2_shaded_gouraud = clampedVec3f((rnd_color_2 * diffuse_gouraud) + Vec3f(ambient), 0.0f, 1.0f); 
+        Vec3f rnd_color_3_shaded_gouraud = clampedVec3f((rnd_color_3 * diffuse_gouraud) + Vec3f(ambient), 0.0f, 1.0f);
+
+        std::vector<TrianglePixel> raster_triangle = rasterize_triangle(Vec2f(v0_device.x, v0_device.y), Vec2f(v1_device.x, v1_device.y), Vec2f(v2_device.x, v2_device.y));
+        for (int i = 0; i < raster_triangle.size(); i++)
+        {
+            TrianglePixel pixel = raster_triangle[i];
+            float interpolated_depth = (v0_device.z * pixel.barycentric.x) + (v1_device.z * pixel.barycentric.y) + (v2_device.z * pixel.barycentric.z);
+
+            if (z_buffer[pixel.pixel.y][pixel.pixel.x] <= interpolated_depth)
+            {
+                Vec3f interpolated_normal = ((n0 * pixel.barycentric.x) + (n1 * pixel.barycentric.y) + (n2 * pixel.barycentric.z)).normalize();
+                Vec2f interpolated_uv = clampedVec2f((uv0 * pixel.barycentric.x) + (uv1 * pixel.barycentric.y) + (uv2 * pixel.barycentric.z), 0.0f, 1.0f);
+                
+                float diffuse = interpolated_normal * light_dir;
+                Vec3f gray_shaded = clampedVec3f(Vec3f(diffuse + ambient), 0.0f, 1.0f);
+                Vec3f interpolated_color = clampedVec3f((rnd_color_1 * pixel.barycentric.x) + (rnd_color_2 * pixel.barycentric.y) + (rnd_color_3 * pixel.barycentric.z), 0.0f, 1.0f);
+                Vec3f interpolated_color_shaded = clampedVec3f((interpolated_color * diffuse) + ambient, 0.0f, 1.0f);
+                Vec3f single_rnd_color_shaded = clampedVec3f((rnd_color_1 * diffuse) + ambient, 0.0f, 1.0f);
+                Vec3f single_color_shaded = clampedVec3f((RED * diffuse) + ambient, 0.0f, 1.0f);
+
+                Vec2i texture_pixel = Vec2i((texture.get_width() - 1) * interpolated_uv.x, (texture.get_height() - 1) * interpolated_uv.y);
+                Vec3f texture_color = to_vec3fcolor(texture.get(texture_pixel.x, texture_pixel.y));
+
+                Vec3f texture_color_shaded = clampedVec3f((texture_color * diffuse) + Vec3f(ambient), 0.0f, 1.0f);
+                Vec3f texture_color_shaded_gouraud = clampedVec3f((texture_color * diffuse_gouraud) + Vec3f(ambient), 0.0f, 1.0f);
+
+                // image.set(pixel.pixel.x, pixel.pixel.y, to_tgacolor(texture_color_shaded_gouraud));
+                // image.set(pixel.pixel.x, pixel.pixel.y, to_tgacolor(texture_color_shaded));
+                // image.set(pixel.pixel.x, pixel.pixel.y, to_tgacolor(interpolated_color_shaded));
+                // image.set(pixel.pixel.x, pixel.pixel.y, to_tgacolor(single_rnd_color_shaded));
+                image.set(pixel.pixel.x, pixel.pixel.y, to_tgacolor(single_color_shaded));
+                z_buffer[pixel.pixel.y][pixel.pixel.x] = interpolated_depth;
+            }
+        }
 
         // draw_line(v0_device, v1_device, image, RED);
         // draw_line(v1_device, v2_device, image, RED);
@@ -132,19 +162,25 @@ int main()
 }
 
 
+Vec3f to_vec3fcolor(TGAColor color)
+{
+    return Vec3f(color.r, color.g, color.b) * (1.0f / 255.0f);
+}
+
+
 TGAColor to_tgacolor(Vec3f color)
 {
     return TGAColor(int(color.x * 255.99), int(color.y * 255.99), int(color.z * 255.99), 255);
 }
 
 
-/**
- * Vertices should be in DEVICE COORDINATES.
- * Bottom left of device is origin.
- */
-void draw_line(Vec3f p0, Vec3f p1, TGAImage& image, Vec3f color)
+// Rastersizes line
+// Points must be in device coordinates
+// Thickness is in pixels
+// KNOWN BUG: bounding box clips thickness.
+std::vector<LinePixel> rasterize_line(Vec2f p0, Vec2f p1, float thickness)
 {
-    // BUG TO FIX: bounding box also clips thickness!!!
+    std::vector<LinePixel> pixels;
 
     // Bounding box
     Vec2i min, max;
@@ -155,37 +191,37 @@ void draw_line(Vec3f p0, Vec3f p1, TGAImage& image, Vec3f color)
     // Clip bounding box
     min.x = std::max(min.x, 0);
     min.y = std::max(min.y, 0);
-    max.x = std::min(max.x, width - 1);
-    max.y = std::min(max.y, height - 1);
+    max.x = std::min(max.x, device_width - 1);
+    max.y = std::min(max.y, device_height - 1);
 
     Vec2f dir = Vec2f(p1.x - p0.x, p1.y - p0.y).normalize();
-    Vec2f orig = Vec2f(p0.x, p0.y);
+    float line_length = (p1 - p0).length();
+    float half_thickness = thickness / 2.0f;
 
     for (int row = min.y; row <= max.y; row++)
     {
         for (int col = min.x; col <= max.x; col++)
         {
             Vec2f p (col + 0.5f, row + 0.5f);
-            float perp_distance = ((p - orig) - (dir * ((p - orig) * dir))).length();
-            float half_thickness = 0.5f;
+            float projected_distance = (p - p0) * dir;
+            float perp_distance = ((p - p0) - (dir * projected_distance)).length();
 
             if (perp_distance <= half_thickness)
             {
-                image.set(col, row, to_tgacolor(color));
+                pixels.push_back(LinePixel {Vec2i(col, row), (1.0f - (projected_distance / line_length))});
             }
         }
     }
+
+    return pixels;
 }
 
 
-/**
- * Vertices should be in DEVICE COORDINATES.
- * Bottom left of device is origin.
- */
-// void draw_triangle(Vec3f a, Vec3f b, Vec3f c, TGAImage& image, Vec3f a_color, Vec3f b_color, Vec3f c_color, float** z_buffer, TGAImage& texture)
-void draw_triangle(Vec3f a, Vec3f b, Vec3f c, TGAImage& image, Vec2f a_uv, Vec2f b_uv, Vec2f c_uv, float** z_buffer, TGAImage& texture)
+// Rastersizes a triangle
+// Points must be in device coordinates
+std::vector<TrianglePixel> rasterize_triangle(Vec2f a, Vec2f b, Vec2f c)
 {
-    // QUESTION: are positive depth values in front of screen? if so should they be culled?
+    std::vector<TrianglePixel> pixels;
 
     // Bounding box for triangle (pixel grid coordinate system)
     Vec2i min;
@@ -197,8 +233,8 @@ void draw_triangle(Vec3f a, Vec3f b, Vec3f c, TGAImage& image, Vec2f a_uv, Vec2f
     // Clip box to be within pixel grid
     min.x = std::max(min.x, 0);
     min.y = std::max(min.y, 0);
-    max.x = std::min(max.x, width - 1);
-    max.y = std::min(max.y, height - 1);
+    max.x = std::min(max.x, device_width - 1);
+    max.y = std::min(max.y, device_height - 1);
 
     Mat3x3f D (Vec3f(a.x, a.y, 1.0f), Vec3f(b.x, b.y, 1.0f), Vec3f(c.x, c.y, 1.0f));
     float determinant = D.determinant();
@@ -206,7 +242,7 @@ void draw_triangle(Vec3f a, Vec3f b, Vec3f c, TGAImage& image, Vec2f a_uv, Vec2f
     if (std::abs(determinant) < 1e-5f)
     {
         std::cout << "degenerate triangle, you little bastard!\n"; // for testing, haven't seen it log once yet
-        return;
+        return pixels;
     };
 
     for (int row = min.y; row <= max.y; row++)
@@ -224,22 +260,10 @@ void draw_triangle(Vec3f a, Vec3f b, Vec3f c, TGAImage& image, Vec2f a_uv, Vec2f
             // Inside triangle check
             if (alpha >= 0.0f && beta >= 0.0f && gamma >= 0.0f)
             {
-                float interpolated_depth = (a.z * alpha) + (b.z * beta) + (c.z * gamma);
-
-                // Depth check
-                if (z_buffer[row][col] < interpolated_depth)
-                {
-                    // Vec3f interpolated_color = clampedVec3f((a_color * alpha) + (b_color * beta) + (c_color * gamma), 0.0f, 1.0f);
-                    // image.set(col, row, to_tgacolor(interpolated_color));
-
-                    Vec2f interpolated_uv = clampedVec2f((a_uv * alpha) + (b_uv * beta) + (c_uv * gamma), 0.0f, 1.0f);
-                    Vec2i texture_pixel = Vec2i((texture.get_width() - 1) * interpolated_uv.x, (texture.get_height() - 1) * interpolated_uv.y);
-                    TGAColor texture_color = texture.get(texture_pixel.x, texture_pixel.y);
-                    image.set(col, row, texture_color);
-
-                    z_buffer[row][col] = interpolated_depth;
-                }
+                pixels.push_back(TrianglePixel {Vec2i(col, row), Vec3f(alpha, beta, gamma)});
             }
         }
     }
+
+    return pixels;
 }
